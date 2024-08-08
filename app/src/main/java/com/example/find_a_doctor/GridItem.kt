ea7ad45bb297868bc98dbb66983dfe9d42866d4f
Abdullah Malik <115897755+MalicKAbdullah@users.noreply.github.com>
2024-08-08 15:10:04 +0500

    HomePage Header/Footer

--- app/src/main/java/com/example/find_a_doctor/GridItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.find_a_doctor
-
-class GridItem(var image: Any, var s: String) {
-
-
-}
--- app/src/main/java/com/example/find_a_doctor/MainActivity.kt
+++ app/src/main/java/com/example/find_a_doctor/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.find_a_doctor
 
 import android.os.Bundle
-import android.widget.GridLayout
 import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+
 import androidx.core.view.GravityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
+@Suppress("DEPRECATION")
 class MainActivity : ComponentActivity() {
     private lateinit var drawerLayout: DrawerLayout
 
@@ -29,23 +24,7 @@ class MainActivity : ComponentActivity() {
             drawerLayout.openDrawer(GravityCompat.START)
         }
 
-        val gridLayout: GridLayout = findViewById(R.id.gridLayout)
-        val items = listOf(
-            GridItem(R.drawable.ic_video_consultation, "Video Consultation"),
-            GridItem(R.drawable.ic_clinic_visit, "In-Clinic Visit"),
-            // Add more items
-        )
 
-        for (item in items) {
-            val itemView = layoutInflater.inflate(R.layout.item_layout, gridLayout, false)
-            val imageView: ImageView = itemView.findViewById(R.id.item_image)
-            val textView: TextView = itemView.findViewById(R.id.item_text)
-
-            imageView.setImageResource(item.image as Int)
-            textView.text = item.s
-
-            gridLayout.addView(itemView)
-        }
     }
 
     override fun onBackPressed() {
