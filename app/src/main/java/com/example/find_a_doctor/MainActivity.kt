c4b34eeefd76a1c3392fc2d52441d2579b17563b
Abdullah Malik <115897755+MalicKAbdullah@users.noreply.github.com>
2024-08-13 01:34:07 +0500

    Complete HomePage

--- app/src/main/java/com/example/find_a_doctor/MainActivity.kt
+++ app/src/main/java/com/example/find_a_doctor/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.find_a_doctor
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -18,6 +20,12 @@ import java.sql.Array
 @Suppress("DEPRECATION")
 class MainActivity : ComponentActivity() {
     private lateinit var drawerLayout: DrawerLayout
+
+    private fun dpToPx(dp: Int): Int {
+        val density = resources.displayMetrics.density
+        return (dp * density + 0.5f).toInt() // Adding 0.5 for rounding to the nearest pixel
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,14 +56,49 @@ class MainActivity : ComponentActivity() {
 //            diseasesLayout.addView(textView)
 
         // Find the LinearLayout within the HorizontalScrollView
+        val imageContainer: LinearLayout = findViewById(R.id.image_container)
         val symptomsLayout: LinearLayout = findViewById(R.id.symptoms)
         val diseasesLayout: LinearLayout = findViewById(R.id.diseases)
+
 
         // Get LayoutInflater to inflate the item layout
         val inflater = LayoutInflater.from(this)
 
+
+
+// Define the gap in pixels
+        val gapInPixels = 16 // Or a fixed value
+
+// Define image resources
+        val imageResourceIds = listOf(R.drawable.promotion2, R.drawable.promotion, R.drawable.promotion2)
+        val diseaseResourceIds = listOf(R.drawable.disease1, R.drawable.disease2, R.drawable.disease3,R.drawable.disease4, R.drawable.disease5, R.drawable.disease6,R.drawable.disease7, R.drawable.disease8, R.drawable.disease9,R.drawable.disease10)
+
+        val symptomResourceId = diseaseResourceIds.shuffled()
+
+        for (imageResId in imageResourceIds) {
+            // Create and configure the ImageView
+            val imageView = ImageView(this)
+            imageView.layoutParams = LinearLayout.LayoutParams(
+                dpToPx(350),
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+
+
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setImageResource(imageResId)
+
+            // Set margin for the image
+            val params = imageView.layoutParams as LinearLayout.LayoutParams
+            params.setMargins(gapInPixels, 0, gapInPixels, 0) // Left, Top, Right, Bottom margins
+            imageView.layoutParams = params
+
+            // Add the ImageView to the LinearLayout
+            imageContainer.addView(imageView)
+
+        }
+
         // Example of adding symptoms items dynamically
-        for (i in 1..6) {
+        for (i in symptomResourceId) {
             // Inflate the item layout for symptoms
             val itemView = inflater.inflate(R.layout.item_disease, symptomsLayout, false)
 
@@ -64,15 +107,16 @@ class MainActivity : ComponentActivity() {
             val textView = itemView.findViewById<TextView>(R.id.textViewDiseaseName)
 
             // Set data for the views
-            imageView.setImageResource(R.drawable.ic_helpline) // Example drawable
+            imageView.setImageResource(i) // Example drawable
             textView.text = "Symptom"
 
             // Add the inflated item layout to the LinearLayout
             symptomsLayout.addView(itemView)
         }
 
+
         // Example of adding items dynamically
-        for (i in 1..10) {
+        for (i in diseaseResourceIds) {
             // Inflate the item layout
             val itemView = inflater.inflate(R.layout.item_disease, diseasesLayout, false)
 
@@ -81,7 +125,7 @@ class MainActivity : ComponentActivity() {
             val textView = itemView.findViewById<TextView>(R.id.textViewDiseaseName)
 
             // Set data for the views
-            imageView.setImageResource(R.drawable.ic_helpline) // Example drawable
+            imageView.setImageResource(i) // Example drawable
             textView.text = "Disease"
 
             // Add the inflated item layout to the LinearLayout
